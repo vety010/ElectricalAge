cd65bf8827da9b23314477a053dd41f38c7755b8
PatriikPlays <61064909+PatriikPlays@users.noreply.github.com>
2022-08-12 01:21:47 +0200

    upgrades to the eln probe

--- src/main/java/mods/eln/simplenode/computerprobe/ComputerProbeEntity.java
+++ src/main/java/mods/eln/simplenode/computerprobe/ComputerProbeEntity.java
@@ -1,6 +1,7 @@
 package mods.eln.simplenode.computerprobe;
 
 import cpw.mods.fml.common.Optional;
+import li.cil.oc.api.driver.DeviceInfo;
 import li.cil.oc.api.machine.Arguments;
 import li.cil.oc.api.machine.Callback;
 import li.cil.oc.api.machine.Context;
@@ -9,23 +10,32 @@ import mods.eln.Other;
 import mods.eln.node.simple.SimpleNodeEntity;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Optional.Interface(iface = "li.cil.oc.api.network.SimpleComponent", modid = Other.modIdOc)
-public class ComputerProbeEntity extends SimpleNodeEntity implements SimpleComponent {
+public class ComputerProbeEntity extends SimpleNodeEntity implements SimpleComponent, DeviceInfo {
 
     public ComputerProbeEntity() {
         super("ElnProbe");
+        deviceInfo.put(DeviceAttribute.Class, DeviceClass.Communication);
+        deviceInfo.put(DeviceAttribute.Description, "Signal Probe");
+        deviceInfo.put(DeviceAttribute.Vendor, "Jrddunbr & The Electrical Age Corporation");
+        deviceInfo.put(DeviceAttribute.Product, "ELN Signal probe");
+        deviceInfo.put(DeviceAttribute.Serial, getNodeUuid().split("-")[0]);
+    }
+
+    private HashMap<String, String> deviceInfo = new HashMap<>();
+
+    @Override
+    public Map<String, String> getDeviceInfo() {
+        return deviceInfo;
     }
 
     @Override
     public String getComponentName() {
         return "ElnProbe";
     }
-
-  /*  @Callback
-    @Optional.Method(modid = Other.modIdOc)
-    public Object[] greet(Context context, Arguments args) {
-        return new Object[]{String.format("Hello, %s!", args.checkString(0))};
-    }*/
 
     @Callback
     @Optional.Method(modid = Other.modIdOc)
@@ -98,6 +108,16 @@ public class ComputerProbeEntity extends SimpleNodeEntity implements SimpleCompo
         if (n == null) return null;
         return n.wirelessGet(context, args);
     }
+
+    @Callback
+    @Optional.Method(modid = Other.modIdOc)
+    public Object[] signalList(Context context, Arguments args) {
+        ComputerProbeNode n = getNode();
+        if (n == null) return null;
+        return n.signalList();
+    }
+
+
 
     public ComputerProbeNode getNode() {
         return (ComputerProbeNode) super.getNode();
